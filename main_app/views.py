8da5137e78d12356a61da954ab6f7aa09791c949
SiddharthNand <sidharthnand47@gmail.com>
2022-12-29 20:52:43 -0800

    Part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+class Dog: 
+  def __init__(self, name, breed, age):
+    self.name = name
+    self.breed = breed
+    self.age = age
+
+dogs = [
+  Dog('Comet', 'golden retriever', 3),
+  Dog('Chico', 'pitbull', 2),
+  Dog('King', 'lab', 4)
+]
+
+
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def dogs_index(request):
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
