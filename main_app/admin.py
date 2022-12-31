8fff0687a09a83ff26dcf71a82289c1c7a51d741
SiddharthNand <sidharthnand47@gmail.com>
2022-12-31 05:26:40 -0800

    part 5

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Dog, Naps
+from .models import Dog, Naps, Toy
 
 admin.site.register(Dog)
 admin.site.register(Naps)
+admin.site.register(Toy)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Dog
-from django.views.generic import ListView
+from .models import Dog, Toy
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import NapsForm
 
@@ -10,10 +10,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
-
-# def dogs_index(request):
-#     dogs = Dog.objects.all()
-#     return render(request, 'dogs/index.html', {"dogs": dogs})
 
 class DogList(ListView):
     model = Dog
@@ -34,16 +30,43 @@ class DogDelete(DeleteView):
 
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
+    id_list = dog.toys.all().values_list('id')
+    toys_dog_doesnt_have = Toy.objects.exclude(id__in=id_list)
     naps_form = NapsForm()
-    return render(request, 'dogs/detail.html', {'dog': dog, 'naps_form': naps_form})
+    return render(request, 'dogs/detail.html', {'dog': dog, 'naps_form': naps_form, 'toys': toys_dog_doesnt_have })
 
 def add_naps(request, dog_id):
   form = NapsForm(request.POST)
-  # validate the form
   if form.is_valid():
-    # don't save the form to the db until it
-    # has the cat_id assigned
     new_naps = form.save(commit=False)
     new_naps.dog_id = dog_id
     new_naps.save()
     return redirect('detail', dog_id=dog_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+
+def assoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = ['name', 'breed', 'age']
